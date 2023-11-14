c1fbee6e6b78a206577acf540c6bc6eef72fd861
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2023-11-14 09:42:45 -0700

    updated GUI to accept user input

--- commute_predictor.py
+++ commute_predictor.py
@@ -4,6 +4,19 @@ from bs4 import BeautifulSoup
 import requests
 import tkinter as tk
 
+grid_depth = 0
+labels = ['Commute Direction',
+        'Max Temp (°C)',
+        'Min Temp (°C)',
+        'Mean Temp (°C)',
+        'Total Rain (mm)',
+        'Total Snow (cm)',
+        'Total Precip (mm)',
+        'Snow on Grnd (cm)',
+        'Wind Direction',
+        'Wind Speed (km/h)']
+root = tk.Tk()
+
 # Loading ColumnTransformer and ML Model
 model_filename = 'commute_estimator_model.pkl'
 with open(model_filename, 'rb') as file:
@@ -11,19 +24,21 @@ with open(model_filename, 'rb') as file:
 
 ct_filename = 'commute_estimator_ct.pkl'
 with open(ct_filename, 'rb') as file:
-    ct = pickle.load(file)
+    ct = pickle.load(file)   
 
-def run_model(r):
-    direction = 'northbound' 
-    max_temp = 10
-    min_temp = 0
-    mean_temp = 5
-    total_rain = 0
-    total_snow = 0
-    total_precip = 0 
-    snow_on_grnd = 0
-    wind_dir = 28
-    wind_speed = 40
+def run_model(inputs): 
+    wind_dir_dict = {'N':0, 'NE': 4.5, 'E':9, 'SE':14.5, 'S':18, 'SW':22.5, 'W':27, 'NW':31.5} 
+
+    direction = 'northbound' if inputs[0].get() else 'southbound'
+    max_temp = float(inputs[1].get())
+    min_temp = float(inputs[2].get())
+    mean_temp = float(inputs[3].get())
+    total_rain = float(inputs[4].get())
+    total_snow = float(inputs[5].get())
+    total_precip = float(inputs[6].get())
+    snow_on_grnd = float(inputs[7].get())
+    wind_dir = wind_dir_dict[inputs[8].get()]
+    wind_speed = float(inputs[16].get())
 
     new_data = pd.DataFrame({
         'direction': [direction],
@@ -37,30 +52,65 @@ def run_model(r):
         'Dir of Max Gust (10s deg)': [wind_dir],
         'Spd of Max Gust (km/h)': [wind_speed]
     })
-
+    
     transformed_new_data = ct.transform(new_data)
     prediction = model.predict(transformed_new_data)
 
-    text = f"It will take approximately {int(prediction//60)} mintues and {int(prediction%60)} seconds to get to school."
+    if direction == 'northbound':
+        text = f"It will take {int(prediction//60)}min, {int(prediction%60)}sec to get to school."
+    else: 
+        text = f"It will take {int(prediction//60)}min, {int(prediction%60)}sec to get home."
 
-    output = tk.Label(r, text=text)
-    output.pack(padx=20, pady=5)
+    output = tk.Label(root, text=text)
+    global grid_depth
+    grid_depth += 1
+    output.grid(row=grid_depth, column=0, columnspan=3, pady=10)
 
-## Try web scraping
-# url = '<a href="https://www.timeanddate.com/weather/canada/calgary">https://www.timeanddate.com/weather/canada/calgary</a>'  # Replace this with the URL of the website you want to scrape
-# response = requests.get(url)
-
-# if response.status_code == 200:
-#     html_content = response.content
-# else:
-#     print("Failed to fetch the website.")
-#     exit()
-
-root = tk.Tk()
 root.title("Commute Predictor")
-root.geometry("600x400")
 
-txt1 = tk.Entry().pack(padx=1, pady=1)
-calc_button = tk.Button(text="Calculate Commute", command=lambda: run_model(root)).pack(padx=5, pady=20)
+entries = []
+commute_direction_var = tk.IntVar()
+selected_wind_direction = tk.StringVar()
+input_vars = []
+
+for label_text in labels:
+    label = tk.Label(root, text=label_text)
+    label.grid(row=grid_depth, column=0, padx=5, pady=5)
+
+    if label_text == 'Commute Direction':
+        northbound_checkbox = tk.Checkbutton(root, text='Northbound', variable=commute_direction_var, onvalue=1, offvalue=0)
+        southbound_checkbox = tk.Checkbutton(root, text='Southbound', variable=commute_direction_var, onvalue=0, offvalue=1)
+
+        northbound_checkbox.grid(row=grid_depth, column=1, padx=5, pady=5, sticky="w")
+        grid_depth += 1
+        southbound_checkbox.grid(row=grid_depth, column=1, padx=5, pady=5, sticky="w")
+
+        input_vars.append(commute_direction_var)
+
+    elif label_text == 'Wind Direction':
+        directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+        checkboxes_per_row = 2
+        for j, direction in enumerate(directions):
+            radiobutton = tk.Radiobutton(root, text=direction, variable=selected_wind_direction, value=direction)
+            row_value = grid_depth + j // checkboxes_per_row
+            col_value = 1 + j % checkboxes_per_row
+            radiobutton.grid(row=row_value, column=col_value, padx=1, pady=5, sticky="w")
+            input_vars.append(selected_wind_direction)
+
+        grid_depth += (len(directions) + checkboxes_per_row - 1) // checkboxes_per_row
+
+    else:
+        entry = tk.Entry(root)
+        entry.grid(row=grid_depth, column=1, columnspan=2, padx=5, pady=5)
+        entries.append(entry)
+
+        input_vars.append(entry)
+
+    grid_depth += 1
+
+calc_button = tk.Button(root, text="Calculate Commute", command=lambda: run_model(input_vars))
+calc_button.grid(row=grid_depth, column=0, columnspan=3, pady=10)
+
+root.update_idletasks()
 
 root.mainloop()
